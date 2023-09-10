7d26dde84dc24276e05c60496a242862d44ece7b
Denis Pasha <denispasha95@gmail.com>
2023-09-10 20:28:13 +0300

    google login

--- src/main/java/com/example/novini_24/web/pagesController/PagesController.java
+++ src/main/java/com/example/novini_24/web/pagesController/PagesController.java
@@ -2,6 +2,7 @@ package com.example.novini_24.web.pagesController;
 
 import com.example.novini_24.model.Articles;
 import com.example.novini_24.service.ApiService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,13 @@ public class PagesController {
         this.apiService = apiService;
     }
 
+    @GetMapping("/")
+    public String getHome1(Model model ){
+        Articles topHeadlinesBg = apiService.getTopHeadlinesBg();
+
+        model.addAttribute("topHeadlinesBg" , topHeadlinesBg);
+        return "index";
+    }
     @GetMapping("/index")
     public String getHome(Model model){
         Articles topHeadlinesBg = apiService.getTopHeadlinesBg();
@@ -38,7 +46,12 @@ public class PagesController {
     }
 
     @GetMapping("/login")
-    public String getLogin(Model model){
+    public String getLogin(Model model, OAuth2AuthenticationToken auth2AuthenticationToken){
+        OAuth2AuthenticationToken auth2AuthenticationToken1 = auth2AuthenticationToken;
+
+        if (auth2AuthenticationToken != null){
+            return "redirect:/";
+        }
         return "login";
     }
 
